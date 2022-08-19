eaa93ccd93d4ad65662b99185383f1a4fefc375a
曾军杰 <lanfein9@hotmail.com>
2022-08-19 18:14:45 +0800

    feat: 添加useMount和useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import qs from "qs";
 import { List } from "./list";
-import { SearchPanel } from "./search-panel";
-import { cleanObject } from "../../utils/index";
+import { SearchPanel } from "./search-panel"
+import { cleanObject, useDebounce, useMount } from "../../utils/index";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
@@ -13,24 +13,24 @@ export const ProjectListScreen = () => {
 
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
-
+  const debounceParam = useDebounce(param, 200)
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(
       async (res) => {
         if (res.ok) {
           setList(await res.json());
         }
       }
     );
-  }, [param]);
+  }, [debounceParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
-  }, [param]);
+  });
 
   return (
     <>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,38 @@
 // 包含0的特殊情况
+import { useEffect } from 'react';
+import { useState } from 'react';
 const isFalsy = (value) => (value === 0 ? false : !value)
 
 // 清理不含值的对象
 export const cleanObject = (obj) => {
-    const result = {...obj}
+    const result = { ...obj }
     Object.keys(result).forEach(key => {
         const value = result[key]
-        if(isFalsy(value)){
+        if (isFalsy(value)) {
             delete result[key]
         }
     })
     return result
+}
+
+// 加载时的自定义Hook
+export const useMount = callback => {
+    useEffect(() => {
+        callback()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+}
+
+export const useDebounce = (value, delay) => {
+    const [debounceValue, setDebounceValue] = useState(value)
+    useEffect(() => {
+        // 每次value变化,设置一个定时器
+        const timeout = setTimeout(() => setDebounceValue(value), delay)
+        // 每次在上一个useEffect处理完在执行
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [delay, value])
+    return debounceValue
+
 }
